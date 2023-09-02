921321e16083d792f138059a6a40da2e274fbfc5
Aditya kumar <120125556+adityakumarcs@users.noreply.github.com>
2023-09-02 12:53:53 +0530

    Add files via upload

---
+++ a.java
@@ -0,0 +1,12 @@
+/**
+ * a
+ */
+public class a {
+
+    public static void main(String[] args) {
+        int a =10;
+        int b =a++;
+        System.out.println(a);
+        System.out.println(b);
+}
+}
